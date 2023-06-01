05f7e960fd19b5a11f308e12bd5d402ba4531b56
absol21 <2006ermek2006@gmail.com>
2023-06-01 19:32:51 +0600

    .

--- main/models.py
+++ main/models.py
@@ -4,3 +4,10 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.name
